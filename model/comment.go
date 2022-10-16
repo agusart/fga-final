8f3f2df53ca830ad844ead8f13251ae1925a28fa
Agus Budianto <agus.kbk@gmail.com>
2022-10-16 15:37:20 +0700

    add Comment model validation

--- model/comment.go
+++ model/comment.go
@@ -6,7 +6,7 @@ type Comment struct {
 	ID        uint `gorm:"primaryKey"`
 	UserID    uint
 	PhotoID   uint
-	Message   string
+	Message   string `json:"message" valid:"required~message is blank"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
